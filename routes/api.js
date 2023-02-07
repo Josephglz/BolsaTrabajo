bc06b01a18a27f8d75924c9937f35625683a3612
JosephGlz <joseph220199@gmail.com>
2023-02-06 21:18:03 -0600

    Careers CRUD

--- routes/api.js
+++ routes/api.js
@@ -36,4 +36,94 @@ router.post('/api/users/auth/validate', async (req, res) => {
     }
 })
 
+router.post('/api/careers/get', async (req, res) => {
+    try {
+        let result = (await db.careers.getData(req.body.id)).data
+        if(result.length) {
+            res.json({
+                status: 1,
+                data: result[0]
+            })
+        } else {
+            res.json({
+                status: 0,
+                message: 'No hay carreras registradas'
+            })
+        }
+    } catch (error) {
+        return {
+            status: 0,
+            message: error.message
+        }
+    }
+})
+
+router.post('/api/careers/store', async (req, res) => {
+    try {
+        const result = await db.careers.store(req.body);
+        if(result.status) {
+            res.json({
+                status: 1,
+                message: 'Carrera agregada correctamente'
+            })
+        } else {
+            res.json({
+                status: 0,
+                message: result.message
+            })
+        }
+    } catch (error) {
+        return {
+            status: 500,
+            message: error.message
+        }
+    }
+
+})
+
+router.post('/api/careers/update', async (req, res) => {
+    try {
+        const result = await db.careers.update(req.body);
+        if(result.status) {
+            res.json({
+                status: 1,
+                message: 'Carrera actualizada correctamente'
+            })
+        } else {
+            res.json({
+                status: 0,
+                message: result.message
+            })
+        }
+    } catch (error) {
+        return {
+            status: 0,
+            message: error.message
+        }
+    }
+})
+
+router.post('/api/careers/delete', async (req, res) => {
+    try {
+        const result = await db.careers.delete(req.body.id);
+        if(result.status) {
+            res.json({
+                status: 1,
+                message: 'Carrera eliminada correctamente'
+            })
+        } else {
+            res.json({
+                status: 0,
+                message: result.message
+            })
+        }
+    } catch (error) {
+        return {
+            status: 500,
+            message: error.message
+        }
+    }
+
+})
+
 module.exports = router
